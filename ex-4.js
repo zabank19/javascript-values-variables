cdfa2da95ec65705046246658f572ec03ff05d6a
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-25 15:25:50 +0700

    Update ex-4.js

--- ex-4.js
+++ ex-4.js
@@ -1,3 +1,8 @@
+//ให้ Comment ต่อท้ายว่าใช่ Expression หรือไม่ตาม Example
+50 + 12; // Expression
+50 + 12; // NOT Expression
+
+//Exercise #4: What is Expression Start coding here
 130 + 300;
 let zero = 1000 - 1000;
 20 * 120;
