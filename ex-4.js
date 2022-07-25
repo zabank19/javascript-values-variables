cfb614363ee5413a1087d74cc4c3ac506386fc8c
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-25 15:11:58 +0700

    updated: ex-4.js
    - Correct mistakes
    - Change the question to match the content.

--- ex-4.js
+++ ex-4.js
@@ -1,14 +1,9 @@
 130 + 300;
-1000 - 1000;
+let zero = 1000 - 1000;
 20 * 120;
 1000 / 2;
 typeof true;
 let name = "John";
 3 > 5;
 10 == 100;
-
-if (10 < 40) {
-  console.log("10 is less than 40");
-}
-
 200;
