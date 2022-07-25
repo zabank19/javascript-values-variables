e56d96dd54b2cb90c840a92263780bb3f05e9f4a
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-25 15:41:30 +0700

    Update ex-4.js

--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,5 @@
-//ให้ Comment ต่อท้ายว่าใช่ Expression หรือไม่ตาม Example
+//ให้ Comment ต่อท้ายถ้าหากว่าเป็น Expression แต่ถ้าหากไม่เป็นให้ปล่อยว่างเอาไว้ตามตัวอย่างนี้
 50 + 12; // Expression
-50 + 12; // NOT Expression
 
 //Exercise #4: What is Expression Start coding here
 130 + 300;
