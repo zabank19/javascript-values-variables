b8f9007f5ab67fecac0ae31347cffce4d164ecae
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-01-02 09:52:09 +0700

    fix(ex-3):check spelling

--- tests/hackHour.test.js
+++ tests/hackHour.test.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 
 describe("Exercise 3", () => {
-  it("fahrenheit should display correct answer", async () => {
+  it("ค่า fahrenheit จะต้องเป็นค่าที่ถูกต้อง", async () => {
     const data = await fs.readFile("./ex-3.js");
 
     const code = `${data} 
